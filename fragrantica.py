f57818c29800bc96556bd686bf599723ad5e7755
Yujee Song <rswfaf@gmail.com>
2021-03-23 22:10:20 +0900

    selenium_crawler

--- fragrantica.py
+++ fragrantica.py
@@ -54,16 +54,16 @@ def main():
                 count += 1
                 t_response = requests.get(t_url, headers=headers)
                 t_html = t_response.content.decode('utf-8').strip('\n')
-                t_soup = BeautifulSoup(t_html, "html.parser")
+                # print(t_html)
+                t_soup = BeautifulSoup(t_html, "lxml")
                 # print(t_soup)
                 graphs = t_soup.select(".grid-x .accord-bar")
-                # print(graph)
                 for g in graphs:
                     graph = " ".join([g.text, g["style"].split("width: ")[-1].split("%")[0]]) + "\n"
                     print(graph)
                     file.write(graph)
 
-                #main-content > div.grid-x.grid-margin-x > div.small-12.medium-12.large-9.cell > div > div:nth-child(2) > div:nth-child(5) > div:nth-child(2) > div > div:nth-child(1) > div.show-for-medium > span
+                # main-content > div.grid-x.grid-margin-x > div.small-12.medium-12.large-9.cell > div > div:nth-child(2) > div:nth-child(5) > div:nth-child(2) > div > div:nth-child(1) > div.show-for-medium > span
                 # moods = t_soup.select(".grid-x .show-for-medium")
                 # moods = t_soup.select("div.grid-x,grid-margin-x div.cell.small-6 .vote-button-legend")
                 # votes = t_soup.select(".grid-x .voting-small-chart-size > div > div")
@@ -73,14 +73,31 @@ def main():
                 #     moodvote = " ".join([moods[index].text, votes[index]["style"]]) + "\n"
                 #     print(moodvote)
                 #     file.write(moodvote)
-                top_note = t_soup.select("#pyramid > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(4) > div > div:nth-child(1) > div:nth-child(2)")
+
+                ##pyramid > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(1) > div:nth-child(2) > a > span
+                top_note = t_soup.find_all("h3")
                 print(top_note)
+                # for note in top_note:
+                #     print(note.text)
 
                 time.sleep(5)
     file.close()
 
             
-
+"""
+<div class="grid-x grid-padding-y" id="pyramid"><div class="cell">
+<div style="display: flex; flex-direction: column; justify-content: center; text-align: center; background: white;">
+<div class="strike-title">
+<h3><span>Fragrance Notes</span></h3></div>
+<pyramid-switch :perfume_id="25529"><pyramid-notes title="Main Notes According to Your Votes"><hr></pyramid-notes>
+<pyramid-level notes="ingredients">
+<div style="display: flex; justify-content: center; text-align: center; flex-wrap: wrap; align-items: flex-end; flex-direction: row; padding: 0.5rem;">
+<div style="margin: 0.2rem; display: flex; justify-content: center; flex-direction: column; text-align: center; opacity: 1; position: relative;">
+<div>
+<img loading="lazy" src="https://fimgs.net/mdimg/sastojci/t.231.jpg" style="width: 5rem;"></div>
+<div><a href="https://www.fragrantica.com/notes/Salt-231.html"><span class="link-span"></span></a>
+Sea Salt</div></div><div style="margin: 0.2rem; display: flex; justify-content: center; flex-direction: column; text-align: center; opacity: 1; position: relative;"><div><img loading="lazy" src="https://fimgs.net/mdimg/sastojci/t.52.jpg" style="width: 4.4rem;"></div><div><a href="https://www.fragrantica.com/notes/Sage-52.html"><span class="link-span"></span></a>Sage</div></div><div style="margin: 0.2rem; display: flex; justify-content: center; flex-direction: column; text-align: center; opacity: 1; position: relative;"><div><img loading="lazy" src="https://fimgs.net/mdimg/sastojci/t.76.jpg" style="width: 4.175rem;"></div><div><a href="https://www.fragrantica.com/notes/Grapefruit-76.html"><span class="link-span"></span></a>Grapefruit</div></div><div style="margin: 0.2rem; display: flex; justify-content: center; flex-direction: column; text-align: center; opacity: 0.97062997469004; position: relative;"><div><img loading="lazy" src="https://fimgs.net/mdimg/sastojci/t.107.jpg" style="width: 3.775rem;"></div><div><a href="https://www.fragrantica.com/notes/Ambrette-Musk-Mallow--107.html"><span class="link-span"></span></a>Ambrette (Musk Mallow)</div></div><div style="margin: 0.2rem; display: flex; justify-content: center; flex-direction: column; text-align: center; opacity: 0.73508893593265; position: relative;"><div><img loading="lazy" src="https://fimgs.net/mdimg/sastojci/t.409.jpg" style="width: 2.5rem;"></div><div><a href="https://www.fragrantica.com/notes/Seaweed-409.html"><span class="link-span"></span></a>Seaweed</div></div></div>
+"""
 
 
 if __name__ == "__main__":
--- selenium_crawler.py
+++ selenium_crawler.py
@@ -1,31 +1,44 @@
+import selenium
 from selenium import webdriver
-from bs4 import BeautifulSoup as soups
-import os
- 
-def search_selenium(search_name, search_path, search_limit) :
-    search_url = "https://www.google.com/search?q=" + str(search_name) + "&hl=ko&tbm=isch"
-    
-    browser = webdriver.Chrome('c:/chromedriver.exe')
-    browser.get(search_url)
-    
-    image_count = len(browser.find_elements_by_tag_name("img"))
-    
-    print("로드된 이미지 개수 : ", image_count)
- 
-    browser.implicitly_wait(2)
- 
-    for i in range( search_limit ) :
-        image = browser.find_elements_by_tag_name("img")[i]
-        path = r"C:\Users\rswfa\Documents\capstone2\Catchinichi-AI\.vscode\imgs"+search_name
-        os.makedirs(path, exist_ok=True)
-        image.screenshot(path + "/" + str(i) + ".png")
- 
-    browser.close()
- 
-if __name__ == "__main__" :
- 
-    search_name = input("검색하고 싶은 키워드 : ")
-    search_limit = int(input("원하는 이미지 수집 개수 : "))
-    search_path = "Your Path"
-    # search_maybe(search_name, search_limit, search_path)
-    search_selenium(search_name, search_path, search_limit)
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
+import time
+
+URL = "https://www.fragrantica.com/perfume/Jo-Malone-London/Wood-Sage-Sea-Salt-25529.html"
+driver = webdriver.Chrome(executable_path=r"C:\Users\rswfa\Documents\capstone2\Catchinichi-AI\chromedriver")
+driver.get(url=URL)
+print(driver.current_url)
+driver.implicitly_wait(time_to_wait=5)
+driver.execute_script("window.scrollTo(0, 1500)") 
+
+# notes_page = driver.find_element_by_css_selector('#pyramid > div.cell > div > div > div > div > div > div > a')
+notes = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[3]/div[1]/div[1]/div/div[5]/div/div[1]/div/div[2]/div[3]/div').find_elements_by_css_selector("div > div > div")
+# print(notes_page.text)
+for note in notes:
+    print(note.text)
+# notes = driver.find_elements_by_css_selector('#pyramid > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(1) > div:nth-child(2)')
+# print(notes)
+
+driver.close()
+
+# options = webdriver.ChromeOptions()
+# options.add_argument('window-size=1920,1080')
+
+# driver = webdriver.Chrome('chromedriver', options=options)
+# driver.implicitly_wait(5)
+
+# driver.get(url='https://www.google.com/')
+
+# search_box = driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
+
+# search_box.send_keys('greeksharifa.github.io')
+# search_box.send_keys(Keys.RETURN)
+
+# posting = driver.find_element_by_xpath('//*[@id="rso"]/div/div[1]/div/div/div[1]/a/h3')
+# posting.click()
+
+# time.sleep(3)
+# driver.close()
