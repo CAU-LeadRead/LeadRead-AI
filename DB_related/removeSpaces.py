1a0116d2b644712b77fcc8bf517f8d876fd93ed0
Yujee Song <rswfaf@gmail.com>
2021-06-08 19:56:49 +0900

    Ignore useless files

--- DB_related/removeSpaces.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-import pandas as pd
-
-fragrance = pd.read_csv("DB_related/fragrance.csv")
-fragrance["brand"] = fragrance["brand"].str.replace(" ", "")
-fragrance["en_name"] = fragrance["en_name"].str.replace(" ", "")
-fragrance.to_csv("DB_related/temp.csv")
